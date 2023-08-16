2305b88a120e45810f57dc557f551323915dc10d
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2023-08-16 16:53:57 +0530

    Time: 0 ms (100%), Space: 40.3 MB (63.32%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
@@ -1,13 +1,11 @@
 class Solution {
     public List<Integer> getRow(int rowIndex) {
-        Integer[] arr = new Integer[rowIndex+1];
-        Arrays.fill(arr, 0);
-        arr[0] = 1;
-        for (int i = 1; i <= rowIndex; i++) {
-            for (int j = i; j > 0 ; j--) {
-                arr[j] = arr[j] + arr[j-1];
-            }
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        for (int j = 0; j < rowIndex; j++) {
+            int temp = (int)(((long)list.get(j) * (rowIndex-j))/(j+1));
+            list.add(temp);
         }
-        return Arrays.asList(arr);
+        return list;
     }
 }
