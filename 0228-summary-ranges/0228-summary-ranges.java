8fb7099def945df3d41f8b824ba6b91dda2a74cb
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2023-08-25 22:49:34 +0530

    Time: 7 ms (12.43%), Space: 40.8 MB (67.51%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -1,35 +1,40 @@
 class Solution {
     public List<String> summaryRanges(int[] nums) {
         List<String> result = new ArrayList<>();
-        List<List<Integer>> list = new ArrayList<>();
-        List<Integer> subList = new ArrayList<>();
         if (nums.length < 1) {
             return result;
         }
         int count = nums[0];
+        int first = nums[0];
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == count) {
-                subList.add(count++);
+                count++;
                 if (i == nums.length-1) {
-                    list.add(subList);
+                    if (first == count-1) {
+                    result.add(Integer.toString(first));
+                    } else {
+                        result.add(first + "->"+(count-1));
+                    }
                 }
             } else {
+                if (first == count-1) {
+                    result.add(Integer.toString(first));
+                } else {
+                    result.add(first + "->"+(count-1));
+                }
                 count = nums[i];
-                list.add(subList);
-                subList = new ArrayList<>();
-                subList.add(count++);
+                first = count++;
                 if (i == nums.length-1) {
-                    list.add(subList);
+                    if (first == count-1) {
+                    result.add(Integer.toString(first));
+                    } else {
+                        result.add(first + "->"+(count-1));
+                    }
                 }
             }
         }
-        for (List<Integer> l1 : list) {
-            if (l1.size() == 1){
-                result.add(Integer.toString(l1.get(0)));
-            } else {
-                String first = l1.get(0) + "->"+l1.get(l1.size()-1);
-                result.add(first);
-            }
+        for (String num : result) {
+            System.out.print(num+" ");
         }
         return result;
     }
