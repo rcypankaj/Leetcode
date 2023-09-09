8f836360680121172eed650542c851622fc91d6d
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2023-09-09 22:34:01 +0530

    Time: 1 ms (99.96%), Space: 43.4 MB (56.93%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,14 +1,15 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int[] arr1 = new int[26];
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] arr = new int[26];
         for (char ch : s.toCharArray()) {
-            arr1[ch - 'a']++;
+            arr[ch - 'a']++;
         }
         for (char ch : t.toCharArray()) {
-            arr1[ch - 'a']--;
-        }
-        for (int i = 0; i < 26; i++) {
-            if (arr1[i] != 0) {
+            arr[ch - 'a']--;
+            if (arr[ch - 'a'] < 0) {
                 return false;
             }
         }
