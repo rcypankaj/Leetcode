ba116041fda2518991ee467d72f6b4fab6c756ba
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2023-09-19 09:34:14 +0530

    Time: 170 ms (12.46%), Space: 44.8 MB (17.79%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,26 +1,24 @@
 class Solution {
     public String reverseVowels(String s) {
         int l = 0, r = s.length()-1;
+        String vowels = "aeiouAEIOU";
         while (l < r) {
             char leftChar = s.charAt(l);
             char rightChar = s.charAt(r);
-            if (isEqualToVowels(leftChar) && isEqualToVowels(rightChar)) {
+            if (vowels.contains(leftChar+"") && vowels.contains(rightChar+"")) {
                 s = swap(s, l, r);
                 l++;
                 r--;
-            } else if (!isEqualToVowels(leftChar) && !isEqualToVowels(rightChar)) {
+            } else if (!vowels.contains(leftChar+"") && !vowels.contains(rightChar+"")) {
                 l++;
                 r--;
-            } else if (isEqualToVowels(leftChar)) {
+            } else if (vowels.contains(leftChar+"")) {
                 r--;
             } else {
                 l++;
             }
         }
         return s;
-    }
-    public boolean isEqualToVowels(char currChar) {
-        return currChar == 'a' || currChar == 'e' || currChar == 'i' || currChar == 'o' || currChar == 'u' || currChar == 'A' || currChar == 'E' || currChar == 'I' || currChar == 'O' || currChar == 'U';
     }
     public String swap (String str, int l, int r) {
         return str.substring(0, l)+ str.charAt(r)+ str.substring(l+1, r) + str.charAt(l)+ str.substring(r+1);
