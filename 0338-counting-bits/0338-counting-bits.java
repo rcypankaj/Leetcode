23206928603e0629a6011f11303d55214361c4fc
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2023-10-21 15:53:14 +0530

    Time: 2 ms (84.15%), Space: 46.5 MB (43.74%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,17 +1,17 @@
 class Solution {
     public int[] countBits(int n) {
-        int[] res = new int[n+1];
-        for (int i = 0; i < n+1; i++) {
-            res[i] = countOnes(i);
+        int res[] = new int[n+1];
+        for(int num = 0; num <= n; num++){
+            res[num] = (bitCnt(num));
         }
         return res;
     }
-    public int countOnes(int num) {
-        int count = 0;
-        while (num > 0) {
-            if ((num&1) == 1) count++;
-            num >>= 1;
+    private int bitCnt(int num){
+         int cnt = 0;
+        while(num > 0){
+            cnt++;
+            num &= (num-1);
         }
-        return count;
-    }
+        return cnt;
+    }    
 }
