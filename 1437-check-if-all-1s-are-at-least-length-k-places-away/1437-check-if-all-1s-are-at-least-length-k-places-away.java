d509eb95f83a903e5224845203fcd64792fd3392
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2024-03-04 23:48:40 +0530

    Time: 1 ms (100.00%), Space: 56.8 MB (16.67%) - LeetHub

---
+++ 1437-check-if-all-1s-are-at-least-length-k-places-away/1437-check-if-all-1s-are-at-least-length-k-places-away.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean kLengthApart(int[] nums, int k) {
+
+	for(int start = -1, i = 0; i < nums.length; i++) 
+		if(nums[i] == 1) {
+			if(start != -1 && i - start - 1 < k) return false;
+			start = i;
+		}
+	return true;
+}
+}
