7c3499488382d9b114f90b5eb86736685ed35b25
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2024-03-15 12:01:00 +0530

    Time: 6 ms (7.88%), Space: 42.9 MB (48.11%) - LeetHub

--- 3046-split-the-array/3046-split-the-array.java
+++ 3046-split-the-array/3046-split-the-array.java
@@ -5,7 +5,6 @@ class Solution {
         for (int num : nums) {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        
         for (Map.Entry<Integer, Integer> e: map.entrySet()) {
             if (e.getValue() <= 2) {
                 count += e.getValue();
