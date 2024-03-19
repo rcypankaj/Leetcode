e9d90edab19ed002e12d53fde23ae90be410f87d
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2024-03-19 23:06:23 +0530

    Time: 2 ms (9.14%), Space: 41.3 MB (74.46%) - LeetHub

--- 1078-occurrences-after-bigram/1078-occurrences-after-bigram.java
+++ 1078-occurrences-after-bigram/1078-occurrences-after-bigram.java
@@ -1,16 +1,15 @@
 class Solution {
-    public String[] findOcurrences(String text, String first, String second) {
-        List<String> str = new ArrayList<>();
-        String[] strArr = text.split(" ");
-        for (int i = 0; i < strArr.length - 1; i++) {
-            if (strArr[i].equals(first) && strArr[i+1].equals(second)) {
-                if (i+2 <= strArr.length-1) str.add(strArr[i+2]);
-            }
+   public String[] findOcurrences(String text, String first, String second) {
+        String[] words = text.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 2; i < words.length; ++i) {
+            if (first.equals(words[i - 2]) && second.equals(words[i - 1]))
+                sb.append(words[i]+" ");
         }
-        String[] res = new String[str.size()];
-        for (int i = 0; i < str.size(); i++) {
-            res[i] = str.get(i);
-        }
+       System.out.println("a"+sb+"b");
+       if (sb.isEmpty()) return new String[]{};
+        System.out.println("a"+sb+"b");
+       String[] res = sb.toString().split(" ");
         return res;
     }
 }
