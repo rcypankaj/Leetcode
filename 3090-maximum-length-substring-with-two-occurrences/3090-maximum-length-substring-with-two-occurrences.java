d18b59078be931b448dc5591c3d4578bd22f46c9
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2024-03-25 23:45:33 +0530

    Time: 1 ms (100.00%), Space: 41.8 MB (66.67%) - LeetHub

--- 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.java
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.java
@@ -1,26 +1,12 @@
 class Solution {
     public int maximumLengthSubstring(String s) {
-        int maxLength = Integer.MIN_VALUE;
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = i+1; j <= s.length(); j++) {
-                // System.out.println(s.substring(i, j));
-                boolean isValid = count(s.substring(i, j));
-                if (isValid)
-                    maxLength = Math.max(s.substring(i, j).length(), maxLength);
-            }
+        int ans = 0; 
+        int[] freq = new int[26]; 
+        for (int i = 0, ii = 0; i < s.length(); ++i) {
+            ++freq[s.charAt(i)-97]; 
+            while (freq[s.charAt(i)-97] == 3) --freq[s.charAt(ii++)-97]; 
+            ans = Math.max(ans, i-ii+1); 
         }
-        return maxLength;
-    }
-    public boolean count(String str) {
-        Map<Character, Integer> charSet = new HashMap<>();
-        for (int i = 0; i < str.length(); i++) {
-            charSet.put(str.charAt(i), charSet.getOrDefault(str.charAt(i), 0) +1);
-        }
-        for (Map.Entry<Character, Integer> e : charSet.entrySet()) {
-            if (e.getValue() > 2) {
-                return false;
-            }
-        }
-        return true;
+        return ans; 
     }
 }
