3d173648b6ca5fd15bbfaf7cea248e763259fbf1
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2024-10-24 10:04:21 +0530

    Time: 5 ms (65.28%), Space: 58.5 MB (21.53%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,4 +1,24 @@
 class Solution {
+    // Find duplicate using likedlist cucle method
+    public int findDuplicate2(int[] nums) {
+        int slow = nums[0], fast = nums[0];
+
+        // Finding the first intersection
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            
+        // We got first intersection
+        } while (slow != fast);
+
+        int slow2 = nums[0];
+        // Finding the second intersection that will be our result
+        while (slow2 != slow) {
+            slow = nums[slow];
+            slow2 = nums[slow2];
+        }
+        return slow;
+    }
     public int findDuplicateUsingBruteForce(int[] nums) {
         int n = nums.length;
         int[] arr = new int[n+1];
@@ -15,6 +35,6 @@ class Solution {
         return -1;
     }
     public int findDuplicate(int[] nums) {
-        return findDuplicateUsingBruteForce(nums);
+        return findDuplicate2(nums);
     }
 }
