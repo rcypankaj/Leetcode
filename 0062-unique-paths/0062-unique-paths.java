464377ca0036ec8ce5279bd9a32db3f2fc6e8dfb
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2024-10-29 10:04:40 +0530

    Time: 0 ms (100%), Space: 40.3 MB (63.57%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,6 +1,4 @@
 class Solution {
-
-    // Works on smalltest cases
     private int findUniquePaths1(int[][] arr, int i, int j, int m, int n) {
         if (i == m-1 && j == n-1) return 1;
         else if (i >= m || j >= n) return 0;
@@ -14,12 +12,23 @@ class Solution {
         int r = m-1;
         double ans = 1;
         for (int i = 1; i <= r; i++) {
-            System.out.println(i);
             ans *= (paths-r+i)/i;
+            System.out.println(i+" "+ans+" "+(paths-r+i)/i);
         }
         return (int)ans;
     }
+       public int uniquePaths4(int m, int n) {
+        int N = n + m - 2;
+        int r = m - 1;
+        double res = 1;
+
+        for(int i = 1; i <= r; i++){
+            res = res * (N-r+i)/i;
+        }
+
+        return (int)res;
+    }
     public int uniquePaths(int m, int n) {
-        return findUniquePaths1(new int[m][n], 0, 0, m, n);
+        return uniquePaths4(m, n);
     }
 }
