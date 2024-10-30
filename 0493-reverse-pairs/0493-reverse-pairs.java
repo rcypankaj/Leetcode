346a76734626ab239ac32e29d6d48feb64a0fa79
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2024-10-30 09:01:23 +0530

    Time: 67 ms (34.93%), Space: 55 MB (48.25%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.java
+++ 0493-reverse-pairs/0493-reverse-pairs.java
@@ -1,75 +1,15 @@
 class Solution {
-      private static void merge(int[] arr, int low, int mid, int high) {
-        ArrayList<Integer> temp = new ArrayList<>(); // temporary array
-        int left = low;      // starting index of left half of arr
-        int right = mid + 1;   // starting index of right half of arr
-
-        //storing elements in the temporary array in a sorted manner//
-
-        while (left <= mid && right <= high) {
-            if (arr[left] <= arr[right]) {
-                temp.add(arr[left]);
-                left++;
-            } else {
-                temp.add(arr[right]);
+    private int countPairs(int[] nums, int si, int mid, int ei) {
+        int count = 0;
+        int right = mid +1;
+        for (int i = si; i <= mid; i++) {
+            while (right <= ei && nums[i] > (long)nums[right]*2) {
                 right++;
             }
+            count += (right-(mid+1));
         }
-
-        // if elements on the left half are still left //
-
-        while (left <= mid) {
-            temp.add(arr[left]);
-            left++;
-        }
-
-        //  if elements on the right half are still left //
-        while (right <= high) {
-            temp.add(arr[right]);
-            right++;
-        }
-
-        // transfering all elements from temporary to arr //
-        for (int i = low; i <= high; i++) {
-            arr[i] = temp.get(i - low);
-        }
+        return count;
     }
-
-    public static int countPairs(int[] arr, int low, int mid, int high) {
-        int right = mid + 1;
-        int cnt = 0;
-        for (int i = low; i <= mid; i++) {
-            while (right <= high && arr[i] > (long)2 * (long)arr[right]) right++;
-            cnt += (right - (mid + 1));
-        }
-        return cnt;
-    }
-
-    public static int mergeSort(int[] arr, int low, int high) {
-        int cnt = 0;
-        if (low >= high) return cnt;
-        int mid = (low + high) / 2 ;
-        cnt += mergeSort(arr, low, mid);  // left half
-        cnt += mergeSort(arr, mid + 1, high); // right half
-        cnt += countPairs(arr, low, mid, high); //Modification
-        merge(arr, low, mid, high);  // merging sorted halves
-        return cnt;
-    }
-
-    public static int team(int[] skill, int n) {
-        return mergeSort(skill, 0, n - 1);
-    }
-    // private int countPairs(int[] nums, int si, int mid, int ei) {
-    //     int count = 0;
-    //     int right = mid +1;
-    //     for (int i = si; i <= mid; i++) {
-    //         while (right <= ei && nums[i] > nums[right]*2) {
-    //             right++;
-    //         }
-    //         count += (right-(mid+1));
-    //     }
-    //     return count;
-    // }
     private void conquer(int[] nums, int si, int mid, int ei) {
         int[] merger = new int[(ei-si)+1];
         int idx1 = si;
@@ -110,8 +50,8 @@ class Solution {
         return count;
     }
     public int reversePairs(int[] nums) {
-        // int cou =  findReversePairs1(nums, 0, nums.length-1);
-        // System.out.println(Arrays.toString(nums));
-        return team(nums, nums.length);
+        int cou =  findReversePairs1(nums, 0, nums.length-1);
+        System.out.println(Arrays.toString(nums));
+        return cou;
     }
 }
