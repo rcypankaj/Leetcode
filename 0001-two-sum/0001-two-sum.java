b87892268e2e1bcb42e62f55a00e4d91240b6b4b
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2024-10-31 20:16:33 +0530

    Time: 2 ms (98.84%), Space: 45.2 MB (10.29%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,6 +1,6 @@
 class Solution {
     // Brute force
-    public int[] findTwoSum1(int[] nums, int target) {
+    private int[] findTwoSum1(int[] nums, int target) {
         int n = nums.length;
         for (int i = 0; i < n-1; i++) {
             for (int j = i+1; j < n; j++) {
@@ -9,7 +9,17 @@ class Solution {
         }
         return new int[]{};
     }
+    // Better Approach
+    private int[] findTwoSum2(int[] nums, int target) {
+        int n = nums.length;
+        Map<Integer, Integer> freq = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            if (freq.containsKey(target - nums[i])) return new int[]{freq.get(target - nums[i]), i};
+            else freq.put(nums[i], i);
+        }
+        return new int[]{};
+    }
     public int[] twoSum(int[] nums, int target) {
-        return findTwoSum1(nums, target);
+        return findTwoSum2(nums, target);
     }
 }
