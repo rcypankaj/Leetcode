aef5d13e975b43d26356cde5d535310305713c08
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2024-10-31 22:07:29 +0530

    Time: 18 ms (61.23%), Space: 44.1 MB (38.4%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -29,24 +29,53 @@ class Solution {
         Set<List<Integer>> ans = new HashSet<>();
         for (int i = 0; i < n-3; i++) {
             for (int j = i+1; j < n-2; j++) {
-                Set<Long> fourth = new HashSet<>();
+                Set<Long> third = new HashSet<>();
                 for (int k = j+1; k < n; k++) {
                     long sum = nums[i]+nums[j];
                     sum += nums[k];
-                    if (fourth.contains(target - sum)) {
+                    if (third.contains(target - sum)) {
                         List<Integer> temp = Arrays.asList(nums[i], nums[j], (int)(target-sum), nums[k]);
                         Collections.sort(temp);
                         ans.add(temp);
                     } 
-                    fourth.add((long)nums[k]);
+                    third.add((long)nums[k]);
                 }
             }
         }
         List<List<Integer>> res = new ArrayList<>(ans);
         return res;
     }
+
+    // Optimal Approach
+    private List<List<Integer>> findFourSum3(int[] nums, int target) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        Set<List<Integer>> set = new HashSet<>();
+        for (int i = 0; i < n-3; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+            for (int j = i+1; j < n-2; j++) {
+                if (j > i+1 && nums[j] == nums[j-1]) continue;
+                int k = j+1;
+                int l = n-1;
+                while (k < l) {
+                    long sum = (long)nums[i]+nums[j]+nums[k]+nums[l];
+                    if (sum == target) {
+                        set.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
+                        k++;
+                        l--;
+                        while (k < l && nums[k] == nums[k-1]) k++;
+                        while (k < l && nums[l] == nums[l+1]) l--;
+                    } else if (sum < target) k++;
+                    else l--;
+                }
+            }
+        }
+        List<List<Integer>> res = new ArrayList<>(set);
+        return res;
+    }
     public List<List<Integer>> fourSum(int[] nums, int target) {
         // return findFourSum1(nums, target);
-        return findFourSum2(nums, target);
+        // return findFourSum2(nums, target);
+        return findFourSum3(nums, target);
     }
 }
