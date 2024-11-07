3c13f6c78998cf39de914a3d751c60a8287c2ebb
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2024-11-07 08:18:06 +0530

    Time: 7 ms (5.2%), Space: 41.7 MB (35.11%) - LeetHub

---
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    private ListNode removeNthFromEnd1(ListNode head, int n) {
+        int size = 0;
+        ListNode temp = head;
+        while (temp != null) {
+            size++;
+            temp = temp.next;
+        }
+
+        int nth = size-n;
+        temp = head;
+        if (nth == 0) return head.next;
+        System.out.println(nth);
+        while (nth >= 0) {
+        System.out.println(nth);
+            if (nth == 1) temp.next = temp.next.next;
+            else temp = temp.next;
+            nth--;
+        }
+        return head;
+    }
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        return removeNthFromEnd1(head, n);
+        // return head;
+    }
+}
