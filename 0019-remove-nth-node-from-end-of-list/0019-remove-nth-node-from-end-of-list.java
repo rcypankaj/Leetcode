6b6ea38a95a82425e90e1e7521adb0e0826aba62
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2024-11-07 08:47:46 +0530

    Time: 0 ms (100%), Space: 41.8 MB (35.11%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -9,17 +9,39 @@
  * }
  */
 class Solution {
+    private ListNode removeNthFromEnd2(ListNode head, int n) {
+        // Keep two variable slow and fast where fast will point to nth node
+        ListNode slow = head, fast = head;
+        int k = n;
+        while (k-- > 0) {
+            fast = fast.next;
+        }
+
+        if (fast == null) return head.next;
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+    }
     private ListNode removeNthFromEnd1(ListNode head, int n) {
-        int size = 0;
         ListNode temp = head;
+        // Coumpute the length
+        int size = 0;
         while (temp != null) {
             size++;
             temp = temp.next;
         }
 
+        // Find the removable node from the start
         int nth = size-n;
+
         temp = head;
+
+        // if size is equal to nth node
         if (nth == 0) return head.next;
+
         while (nth >= 0) {
             if (nth == 1) temp.next = temp.next.next;
             else temp = temp.next;
@@ -28,7 +50,11 @@ class Solution {
         return head;
     }
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        return removeNthFromEnd1(head, n);
-        // return head;
+        // Brute Force
+        // return removeNthFromEnd1(head, n);
+
+        // Optimal Approach
+        return removeNthFromEnd2(head, n);
+
     }
-}
+} 
