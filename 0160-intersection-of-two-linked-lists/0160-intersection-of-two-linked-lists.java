03d2a88add942f5cdad52b35658655e118306d41
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2024-11-09 23:45:09 +0530

    Time: 1 ms (99.92%), Space: 48.6 MB (47.01%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -18,6 +18,24 @@ public class Solution {
         }
     }
 
+    private ListNode intersectionNode4(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+
+        ListNode pointer1 = headA;
+        ListNode pointer2 = headB;
+
+        // Traverse both lists until the pointers either meet or reach the end.
+        while (pointer1 != pointer2) {
+            // Move each pointer to the start of the other list upon reaching the end.
+            // This will align them at the same number of steps from the intersection (if one exists).
+            pointer1 = (pointer1 == null) ? headB : pointer1.next;
+            pointer2 = (pointer2 == null) ? headA : pointer2.next;
+        }
+
+        // Return the intersection node or null if no intersection exists.
+        return pointer1;
+    }
+
     private int getDifference(ListNode headA, ListNode headB) {
         int len1 = 0, len2 = 0;
         ListNode currA = headA, currB = headB;
@@ -89,6 +107,8 @@ public class Solution {
 
         // return intersectionNode2(headA, headB);
 
-        return intersectionNode3(headA, headB);
+        // return intersectionNode3(headA, headB);
+
+        return intersectionNode4(headA, headB);
     }
 }
