a1ad481b8e80ed6f297634f57c0d86e5dd5904fc
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2024-11-11 20:02:58 +0530

    Time: 4 ms (84.42%), Space: 69.1 MB (13.89%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,6 +9,56 @@
  * }
  */
 class Solution {
+    private void printList(ListNode head) {
+        ListNode it = head;
+        while (it != null) {
+            System.out.print(it.val+" ");
+            it = it.next;
+        }
+    }
+    private ListNode reverseLinkedList(ListNode head) {
+        ListNode prev = null, it = head;
+
+        while (it != null) {
+            ListNode nextNode = it.next;
+            // printList(it);
+            // System.out.println();
+            it.next = prev;
+            prev = it;
+            it = nextNode;
+        }
+        return prev;
+    }
+    
+    private boolean isPalindrome2(ListNode head) {
+
+        if (head == null || head.next == null) return true;
+        
+        ListNode slow = head, fast = head;
+        // Find middle node
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Get the reversed linked list
+        ListNode newHead = reverseLinkedList(slow);
+        ListNode slow1 = head, slow2 = newHead;
+
+        while (slow2 != null) {
+            // return false slow1 val != slow2 val 
+            if (slow1.val != slow2.val) {
+                // Get back the list to its original state
+                reverseLinkedList(newHead); 
+                return false;
+            }
+            slow1 = slow1.next;
+            slow2 = slow2.next;
+        }
+        // Get back the list to its original state
+        reverseLinkedList(newHead); 
+        return true;
+    }
     private boolean isPalindrome1(ListNode head) {
         ArrayList<Integer> list = new ArrayList<>();
         ListNode it = head;
@@ -29,6 +79,9 @@ class Solution {
     }
     public boolean isPalindrome(ListNode head) {
         // Brute force 
-        return isPalindrome1(head);
+        // return isPalindrome1(head);
+
+        // Optimal approach
+        return isPalindrome2(head);
     }
 }
