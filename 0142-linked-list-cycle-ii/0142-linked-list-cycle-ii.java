fd81a303755e5897137c1d4836fe38f50957b6b2
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2024-11-12 08:44:50 +0530

    Time: 0 ms (100%), Space: 44.4 MB (56.43%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -10,13 +10,15 @@
  * }
  */
 public class Solution {
-    private ListNode detectCycel2(ListNode head) {
-
+    private ListNode detectCycle2(ListNode head) {
+        // Set up slow and fast for finding the collision
         ListNode slow = head, fast = head;
+
         while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
             
+            // If we found the collision than find the first node of the cycle
             if (slow == fast) {
                 ListNode slow2 = head;
                 while (slow != slow2) {
@@ -26,24 +28,27 @@ public class Solution {
                 return slow2;
             };
         }
+        // There is no cycle
         return null;
     }
     private ListNode detectCycle1(ListNode head) {
+        // Set up HashSet for tracking the Node frequency
         HashSet<ListNode> set = new HashSet<>();
         ListNode it = head;
-        int count = 0;
         while (it != null) {
-            if (set.contains(it)) {
-                return it;
-            } else {
-                set.add(it);
-            }
+            // We found the first node of the cycle
+            if (set.contains(it)) return it;
+            else set.add(it);
             it = it.next;
         }
+        // There is no cycle
         return null;
     }
     public ListNode detectCycle(ListNode head) {
+        // Brute force
         // return detectCycle1(head);
-        return detectCycel2(head);
+
+        // Optimal approach
+        return detectCycle2(head);
     }
 }
