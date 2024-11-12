384342c90d725e974ea1625fb6e4f7142b726485
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2024-11-12 08:37:23 +0530

    Time: 0 ms (100%), Space: 44.4 MB (71.31%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -10,6 +10,24 @@
  * }
  */
 public class Solution {
+    private ListNode detectCycel2(ListNode head) {
+
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if (slow == fast) {
+                ListNode slow2 = head;
+                while (slow != slow2) {
+                    slow = slow.next;
+                    slow2 = slow2.next;
+                }
+                return slow2;
+            };
+        }
+        return null;
+    }
     private ListNode detectCycle1(ListNode head) {
         HashSet<ListNode> set = new HashSet<>();
         ListNode it = head;
@@ -25,6 +43,7 @@ public class Solution {
         return null;
     }
     public ListNode detectCycle(ListNode head) {
-        return detectCycle1(head);
+        // return detectCycle1(head);
+        return detectCycel2(head);
     }
 }
