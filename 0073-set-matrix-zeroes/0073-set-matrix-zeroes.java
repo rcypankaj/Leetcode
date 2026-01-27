f70e5a51bd87f79e013776a260d5f57fe8bade39
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2026-01-27 23:54:12 +0530

    Time: 0 ms (100%), Space: 47.7 MB (10.55%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,39 +1,23 @@
 class Solution {
-    public void entireRow(int[][] arr, int i, int j, boolean[][] flag) {
-        int k = 0, m = arr[0].length;
-        while (k < m) {
-            if (k > j && arr[i][k] == 0) {
-                k++;
-                continue;
-            }
-            flag[i][k++] = true;
-        }
-    }
-    public void entireCol(int[][] arr, int i, int j, boolean[][] flag) {
-        int k = 0, n = arr.length;
-        while (k < n) {
-            if (k > i && arr[k][j] == 0) {
-                k++;
-                continue;
-            }
-            flag[k++][j] = true;
-        }
-    }
     public void setZeroes(int[][] matrix) {
-        int n = matrix.length, m = matrix[0].length;
-        boolean[][] flag = new boolean[n][m];
-
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int[] row = new int[n];
+        int[] col = new int[m];
+        Arrays.fill(row, -1);
+        Arrays.fill(col, -1);
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (matrix[i][j] == 0 && flag[i][j] != true) {
-                    entireRow(matrix, i, j, flag);
-                    entireCol(matrix, i, j, flag);
+                if (matrix[i][j] == 0) {
+                    row[i] = 0;
+                    col[j] = 0;
                 }
             }
         }
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (flag[i][j] == true) {
+                if (row[i] == 0 || col[j] == 0) {
                     matrix[i][j] = 0;
                 }
             }
