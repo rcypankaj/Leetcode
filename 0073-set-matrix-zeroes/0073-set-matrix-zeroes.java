a47b3d1c4021a17f853563f2c35743f6daec368b
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2026-01-28 00:45:52 +0530

    Time: 0 ms (100%), Space: 47.7 MB (10.55%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -2,25 +2,29 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int n = matrix.length;
         int m = matrix[0].length;
-        int[] row = new int[n];
-        int[] col = new int[m];
-        Arrays.fill(row, -1);
-        Arrays.fill(col, -1);
+        boolean zeroinFirstRow = false;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (matrix[i][j] == 0) {
-                    row[i] = 0;
-                    col[j] = 0;
+                    if (i == 0) {
+                        zeroinFirstRow = true;
+                    } else {
+                        matrix[0][j] = 0;
+                        matrix[i][0] = 0;
+                    }
                 }
             }
         }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (row[i] == 0 || col[j] == 0) {
+        for (int i = n-1; i >= 1; i--) {
+            for (int j = m-1; j >= 0; j--) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
             }
         }
+        // System.out.println(zeroinFirstRow + "zeroinFirstRow");
+        if (zeroinFirstRow)
+            for (int i = 0; i < m; i++)
+                matrix[0][i] = 0;
     }
 }
