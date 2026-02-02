b3ca44255ede1e70c5209a945686dd6fb3bcc926
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2026-02-02 23:45:09 +0530

    Time: 1 ms (94.59%), Space: 43.8 MB (11.96%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,21 +1,31 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> triangle = new ArrayList<>();
-
-        for (int i = 0; i < numRows; i++) {
-            triangle.add(getNthRow(i));
-        }
-        return triangle;
-    }
-    private List<Integer> getNthRow(int n) {
-        List<Integer> row = new ArrayList<>();
-        int val = 1;
-        row.add(val);
-
-        for (int k = 1; k <= n; k++) {
-            val = val * (n - k + 1) / k;
-            row.add(val);
-        }
-        return row;
+        // Create an array list to store the output result...
+        List<List<Integer>> output = new ArrayList<List<Integer>>();
+        // Base cases...
+	    if (numRows <= 0)  return output;
+        // Create an array list to store the prev triangle value for further addition...
+	    ArrayList<Integer> prev = new ArrayList<Integer>();
+        // Inserting for the first row & store the prev array to the output array...
+	    prev.add(1);
+	    output.add(prev);
+        // For rest of the rows, Traverse all elements through a for loop...
+	    for (int i = 2; i <= numRows; i++) {
+            // Create another array to store the current triangle value...
+		    ArrayList<Integer> curr = new ArrayList<Integer>();
+		    curr.add(1);    //first
+            // Calculate for each of the next...
+		    for (int j = 0; j < prev.size() - 1; j++) {
+                // Inserting the addition of the prev arry two values...
+			    curr.add(prev.get(j) + prev.get(j + 1));    //middle
+		    }
+            // Store the number 1...
+		    curr.add(1);    //last
+            // Store the value in the Output array...
+		    output.add(curr);
+            // Set prev is equal to curr...
+		    prev = curr;
+	    }
+	    return output;      // Return the output list of pascal values...
     }
 }
