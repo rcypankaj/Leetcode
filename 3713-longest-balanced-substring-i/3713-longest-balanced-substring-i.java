37d04ea1d39ecef42d9ef54f7a15f8b097e5c10d
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2026-02-13 23:53:18 +0530

    Time: 21 ms (99.86%), Space: 47.1 MB (27.38%) - LeetHub

--- 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.java
+++ 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.java
@@ -1,26 +1,67 @@
 class Solution {
+    public boolean isValid(String str) {
+        int n = str.length();
+        if (n <= 1) return true;
+        Map<Character, Integer> freq = new HashMap<>();
+        Set<Character> characters = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            char currChar = str.charAt(i);
+            freq.put(currChar, freq.getOrDefault(currChar, 0)+1);
+            characters.add(currChar);
+        }
+        int defaultVal = freq.get(str.charAt(0));
+        for (Character ch : characters) {
+            if (freq.get(ch) != defaultVal) return false;
+        }
+        return true;
+    }
     public int longestBalanced(String s) {
         int n = s.length();
-        int res = 0;
-        int[] cnt = new int[26];
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(cnt, 0);
-            for (int j = i; j < n; j++) {
-                boolean flag = true;
-                int c = s.charAt(j) - 'a';
-                cnt[c]++;
-                for (int x : cnt) {
-                    if (x > 0 && x != cnt[c]) {
-                        flag = false;
-                        break;
-                    }
-                }
 
-                if (flag) {
-                    res = Math.max(res, j - i + 1);
-                }
+        // Transform char -> int
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) 
+            a[i] = s.charAt(i) - 'a';
+
+        int result = 0;
+        for (int l = 0; l < n; l++) {
+            // Early exit, can't be bigger
+            if (n - l <= result) 
+                break;
+
+            int[] cnt = new int[26]; // Counts of every char
+            int uniq = 0, maxfreq = 0; // Number of uniq chars and maximum frequency
+            for (int r = l; r < n; r++) {
+                int i = a[r];
+
+                // There was no this char before => one more uniq
+                if (cnt[i] == 0) 
+                    uniq++;
+
+                cnt[i]++;
+                // Update max frequency
+                if (cnt[i] > maxfreq) 
+                    maxfreq = cnt[i];
+
+                // Check if all uniq chars have maxfreq frequency then update the result
+                int cur = r - l + 1;
+                if (uniq * maxfreq == cur && cur > result)
+                    result = cur;
             }
         }
-        return res;
+        return result;
     }
+    // public int longestBalanced(String s) {
+    //     int n = s.length();
+    //     int maxLen = 0;
+    //     for (int i = 0; i < n; i++) {
+    //         for (int j = i; j < n; j++) {
+    //             if (isValid(s.substring(i, j+1))) {
+    //                 maxLen = Math.max(maxLen, j-1+1);
+    //                 System.out.println(s.substring(i, j+1)+ " J: "+j+"I: "+i+ maxLen);
+    //             }
+    //         }
+    //     }
+    //     return maxLen;
+    // }
 }
