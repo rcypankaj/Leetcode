dd0c5928687834b4b1b8922f15cf1c40cef815cd
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2026-02-16 00:28:09 +0530

    Time: 62 ms (38.25%), Space: 46.4 MB (34.56%) - LeetHub

--- 2698-find-the-punishment-number-of-an-integer/2698-find-the-punishment-number-of-an-integer.java
+++ 2698-find-the-punishment-number-of-an-integer/2698-find-the-punishment-number-of-an-integer.java
@@ -1,25 +1,31 @@
 class Solution {
+    Boolean memo[][];
     public boolean canSplit(String str, int idx, int target) {
         int n = str.length();
         if (idx == n) {
             return target == 0;
         }
 
+        if (memo[idx][target] != null) return memo[idx][target];
+
         int num = 0;
         for (int j = idx; j < n; j++) {
             int currNum = str.charAt(j) - '0';
             num = num*10+currNum;
-            if (num > target) return false;
+            if (num > target) break;
             if (canSplit(str, j+1, target-num)) {
+                memo[idx][target] = true;
                 return true;
             }
         }
-        return false;
+        return memo[idx][target] = false;
     }
     public int punishmentNumber(int n) {
         int ans = 0;
         for (int i = 1; i <= n; i++) {
-            if (canSplit(Integer.toString(i*i), 0, i)) {
+            String sq = Integer.toString(i*i);
+            memo = new Boolean[sq.length()][i+1];
+            if (canSplit(sq, 0, i)) {
                 ans += (i*i);
             }
         }
