57cf165f2b19020d49eff1b198820402eeaa9d5d
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2026-02-19 00:23:12 +0530

    Time: 0 ms (100%), Space: 43.2 MB (60.63%) - LeetHub

--- 0840-magic-squares-in-grid/0840-magic-squares-in-grid.java
+++ 0840-magic-squares-in-grid/0840-magic-squares-in-grid.java
@@ -1,38 +1,30 @@
 class Solution {
     public boolean isMagic(int[][] grid, int r, int c) {
         boolean[] seen = new boolean[10];
-
-        for (int i = r; i < r + 3; i++) {
-            for (int j = c; j < c + 3; j++) {
+        for (int i = r; i < r+3; i ++) {
+            for (int j = c; j < c+3; j++) {
                 int val = grid[i][j];
-                if (val < 1 || val > 9 || seen[val]) return false;
+                if (val > 9 || val < 1 || seen[val] == true) return false;
                 seen[val] = true;
             }
         }
-
-        for (int i = 0; i < 3; i++) {
-
-            int rowSum = grid[r + i][c]
-                       + grid[r + i][c + 1]
-                       + grid[r + i][c + 2];
-
-            int colSum = grid[r][c + i]
-                       + grid[r + 1][c + i]
-                       + grid[r + 2][c + i];
-
-            if (rowSum != 15 || colSum != 15) return false;
+        for (int i = r; i < r+3; i++) {
+            int rowSum = 0;
+            rowSum = grid[i][c]+ grid[i][c+1] + grid[i][c+2];
+            if (rowSum != 15) return false;
         }
 
-        // diagonals
-        int diag1 = grid[r][c]
-                  + grid[r + 1][c + 1]
-                  + grid[r + 2][c + 2];
+        for (int i = c; i < c+3; i++) {
+            int colSum = 0;
+            colSum = grid[r][i]+ grid[r+1][i] + grid[r+2][i];
+            if (colSum != 15) return false;
+        }
 
-        int diag2 = grid[r][c + 2]
-                  + grid[r + 1][c + 1]
-                  + grid[r + 2][c];
+        int diagonalSum = grid[r][c]+ grid[r+1][c+1]+grid[r+2][c+2];
+        int reverseDiagonal = grid[r][c+2] + grid[r+1][c+1] + grid[r+2][c];
 
-        return diag1 == 15 && diag2 == 15;
+        
+        return diagonalSum == 15 && reverseDiagonal == 15;
     }
     public int numMagicSquaresInside(int[][] grid) {
         int n = grid.length;
