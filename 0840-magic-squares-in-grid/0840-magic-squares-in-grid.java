09425c7fa490efd5dd45e4a69078c444655a75e1
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2026-02-18 23:18:27 +0530

    Time: 0 ms (100%), Space: 43.2 MB (73.34%) - LeetHub

---
+++ 0840-magic-squares-in-grid/0840-magic-squares-in-grid.java
@@ -0,0 +1,50 @@
+class Solution {
+    public boolean isMagic(int[][] grid, int r, int c) {
+        boolean[] seen = new boolean[10];
+
+        // check digits 1..9 unique
+        for (int i = r; i < r + 3; i++) {
+            for (int j = c; j < c + 3; j++) {
+                int val = grid[i][j];
+                if (val < 1 || val > 9 || seen[val]) return false;
+                seen[val] = true;
+            }
+        }
+
+        // check rows and columns
+        for (int i = 0; i < 3; i++) {
+
+            int rowSum = grid[r + i][c]
+                       + grid[r + i][c + 1]
+                       + grid[r + i][c + 2];
+
+            int colSum = grid[r][c + i]
+                       + grid[r + 1][c + i]
+                       + grid[r + 2][c + i];
+
+            if (rowSum != 15 || colSum != 15) return false;
+        }
+
+        // diagonals
+        int diag1 = grid[r][c]
+                  + grid[r + 1][c + 1]
+                  + grid[r + 2][c + 2];
+
+        int diag2 = grid[r][c + 2]
+                  + grid[r + 1][c + 1]
+                  + grid[r + 2][c];
+
+        return diag1 == 15 && diag2 == 15;
+    }
+    public int numMagicSquaresInside(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int count = 0;
+        for (int i = 0; i <= n-3; i++) {
+            for (int j = 0; j <= m-3; j++) {
+                if (isMagic(grid, i, j)) count++;
+            }
+        }
+        return count;
+    }
+}
