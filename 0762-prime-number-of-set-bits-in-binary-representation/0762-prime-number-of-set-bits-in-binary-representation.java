893838d6a22dc69e281b473ad0a46ae49f4d3973
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2026-02-21 23:45:39 +0530

    Time: 2 ms (98.83%), Space: 42.2 MB (90.65%) - LeetHub

--- 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.java
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.java
@@ -1,6 +1,5 @@
 class Solution {
     private static boolean[] prime = new boolean[32]; 
-    // max set bits for 10^6 < 2^20 → at most 20 bits
     
     static {
         Arrays.fill(prime, true);
@@ -15,30 +14,11 @@ class Solution {
             }
         }
     }
-    private boolean isPrime(String str) {
-        int n = str.length();
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (str.charAt(i) == '1') count++;
-        }
-        return prime[count];
-    }
-    private String toBinary(int num) {
-        StringBuilder sb = new StringBuilder();
-        while (num > 0) {
-            if ((num&1) == 1) {
-                sb.append(1);
-            } else sb.append(0);
-            num = num >> 1;
-        }
-        return sb.toString();
-    }
     public int countPrimeSetBits(int left, int right) {
         int res = 0;
         for (int i = left; i <= right; i++) {
-            String binaryNum = toBinary(i);
-            boolean isPrime = isPrime(binaryNum);
-            if (isPrime) {
+            int binaryNum = Integer.bitCount(i);
+            if (prime[binaryNum]) {
                 res++;
             }
         }
