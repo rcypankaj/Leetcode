f4b2f7f9e87f7766e5dce9f62c24378aafe32aee
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2026-03-05 00:42:43 +0530

    Time: 2 ms (94.84%), Space: 47.1 MB (83.45%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.java
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.java
@@ -1,30 +1,23 @@
 class Solution {
-    public boolean isSpecialIdx(int[][] mat, int row, int col) {
-        int n = mat.length, m = mat[0].length;
-        int count1InRow = 0, count1InCol = 0;
-        for (int i = 0; i < m; i++) {
-            if (mat[row][i] == 1) {
-                count1InRow++;
-            }
-        }
-        for (int i = 0; i < n; i++) {
-            if (mat[i][col] == 1 && row != i) {
-                count1InCol++;
-            }
-        }
-        return (count1InRow+count1InCol) == 1;
-    }
     public int numSpecial(int[][] mat) {
+        int n = mat.length, m = mat[0].length;
+        int[] row = new int[n];
+        int[] col = new int[m];
         int res = 0;
-        int n = mat.length;
-        int m = mat[0].length;
-        
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (mat[i][j] == 1)
-                    if (isSpecialIdx(mat, i, j)) {
-                        res++;
-                    }
+                if (mat[i][j] == 1) {
+                    row[i]++;
+                    col[j]++;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (mat[i][j] == 1 && row[i]+col[j] == 2) {
+                    res++;
+                }
             }
         }
         return res;
