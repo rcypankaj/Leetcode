f71c0a457a9085cb0d268899414a79d9d16f6efa
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2026-03-05 14:40:00 +0530

    Time: 4 ms (75.73%), Space: 43.7 MB (79.17%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
@@ -1,25 +1,15 @@
 class Solution {
-    private String formStr(int len, boolean isZero) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < len; i++) {
-            if (isZero) {
-                sb.append(0);
-            } else sb.append(1);
-            isZero = !isZero;
-        }
-        return sb.toString();
-    }
     public int minOperations(String s) {
         int n = s.length();
-        String possibleStr1 = formStr(n, true);
-        String possibleStr2 = formStr(n, false);
         int diffBitStr1 = 0, diffBitStr2 = 0;
         for (int i = 0; i < n; i++) {
             char currChar = s.charAt(i);
-            if (currChar != possibleStr1.charAt(i)) {
+            char possibleCharZero = (i&1) == 1 ? '1' : '0';
+            char possibleCharOne = (i&1) == 1 ? '0' : '1';
+            if (currChar != possibleCharZero) {
                 diffBitStr1++;
             }
-            if (currChar != possibleStr2.charAt(i)) {
+            if (currChar != possibleCharOne) {
                 diffBitStr2++;
             }
         }
