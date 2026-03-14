92e53e7bccce4516e5b049863ebb929388671d03
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2026-03-14 21:25:25 +0530

    Time: 6 ms (52.67%), Space: 44.5 MB (51.65%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -1,24 +1,21 @@
 class Solution {
-    private String ans = "";
     private final char[] arr = new char[]{'a', 'b', 'c'};
-    public void findStr(String curr, int n, int[] k) {
-        if (!ans.isEmpty()) return;
-
+    public String findStr(String curr, int n, int[] k) {
         if (curr.length() == n) {
             k[0]--;
-            if (k[0] == 0) ans = curr;
-            return;
+            if (k[0] == 0) return curr;
+            return "";
         }
 
         for (char ch : arr) {
             if (curr.isEmpty() || curr.charAt(curr.length()-1) != ch) {
-                findStr(curr+ch, n, k);
+                String ans = findStr(curr+ch, n, k);
+                if (!ans.isEmpty()) return ans;
             }
         }
+        return "";
     }
     public String getHappyString(int n, int k) {
-        ans = "";
-        findStr("", n, new int[]{k});
-        return ans;
+        return findStr("", n, new int[]{k});
     }
 }
