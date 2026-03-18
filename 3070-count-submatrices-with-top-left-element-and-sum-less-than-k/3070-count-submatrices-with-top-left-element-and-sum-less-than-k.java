54a2bfa301b6288cb4a38f8f1d339a31e081fb30
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2026-03-18 17:13:42 +0530

    Time: 134 ms (5.65%), Space: 161.4 MB (58.06%) - LeetHub

--- 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.java
+++ 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.java
@@ -1,30 +1,16 @@
 class Solution {
-    public boolean isValid(int[][] grid, int r, int c, int k) {
-        int sum = 0;
-        for (int i = 0; i <= r; i++) {
-            sum += grid[i][c];
-        }
-        return sum <= k;
-    }
     public int countSubmatrices(int[][] grid, int k) {
         int n = grid.length;
         int m = grid[0].length;
         int res = 0;
         for (int i = 0; i < n; i++) {
-            int prev = 0;
             for (int j = 0; j < m; j++) {
-                grid[i][j] += prev;
-                prev = grid[i][j];
-            }
-        }
-
-        for (int j = 0; j < m; j++) {
-            int colSum = 0;
-            for (int i = 0; i < n; i++) {
-                grid[i][j] += colSum;
-                colSum = grid[i][j];
+                if (i > 0) grid[i][j] += grid[i-1][j];
+                if (j > 0) grid[i][j] += grid[i][j-1];
+                if (i > 0 && j > 0) grid[i][j] -= grid[i-1][j-1];
                 if (grid[i][j] <= k) res++;
             }
+            System.out.println(Arrays.toString(grid[i]));
         }
         return res;
     }
