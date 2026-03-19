57ac6581ef0fbf73e60ff31d779c1fbbdf1ca65a
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2026-03-19 09:55:28 +0530

    Time: 39 ms (30.77%), Space: 249.5 MB (65.38%) - LeetHub

--- 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.java
+++ 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.java
@@ -1,14 +1,11 @@
 class Solution {
     public int numberOfSubmatrices(char[][] grid) {
-        int n = grid.length, m = grid[0].length;
         int res = 0;
-
+        int n = grid.length, m = grid[0].length;
         int[][] xCount = new int[n][m];
         int[][] yCount = new int[n][m];
-
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-
                 if (grid[i][j] == 'X') xCount[i][j] = 1;
                 if (grid[i][j] == 'Y') yCount[i][j] = 1;
 
@@ -16,21 +13,20 @@ class Solution {
                     xCount[i][j] += xCount[i-1][j];
                     yCount[i][j] += yCount[i-1][j];
                 }
+
                 if (j > 0) {
                     xCount[i][j] += xCount[i][j-1];
                     yCount[i][j] += yCount[i][j-1];
                 }
+
                 if (i > 0 && j > 0) {
                     xCount[i][j] -= xCount[i-1][j-1];
                     yCount[i][j] -= yCount[i-1][j-1];
                 }
 
-                if (xCount[i][j] > 0 && xCount[i][j] == yCount[i][j]) {
-                    res++;
-                }
+                if (xCount[i][j] > 0 && xCount[i][j] == yCount[i][j]) res++;
             }
         }
-
         return res;
     }
 }
