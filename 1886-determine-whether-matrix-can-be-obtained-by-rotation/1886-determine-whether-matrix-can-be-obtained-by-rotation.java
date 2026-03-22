d6914d8ee9cbe3820bf4ae6eb8f698a0b45aa143
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2026-03-22 16:11:15 +0530

    Time: 0 ms (100%), Space: 43.8 MB (16.29%) - LeetHub

--- 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.java
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.java
@@ -1,14 +1,22 @@
 class Solution {
     private void rotate90(int[][] mat) {
         int n = mat.length;
-        int[][] arr = new int[n][n];
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                arr[j][n-i-1] = mat[i][j];
+            for (int j = i; j < n; j++) {
+                int temp = mat[j][i];
+                mat[j][i] = mat[i][j];
+                mat[i][j] = temp;
             }
         }
-        for (int i = 0; i < n; i++) {
-            System.arraycopy(arr[i], 0, mat[i], 0, n);
+        for (int r = 0; r < n; r++) {
+            int i = 0, j = n-1;
+            while (i < j) {
+                int temp = mat[r][i];
+                mat[r][i] = mat[r][j];
+                mat[r][j] = temp;
+                i++;
+                j--;
+            }
         }
     }
     private boolean isEqual(int[][] mat, int[][] target) {
