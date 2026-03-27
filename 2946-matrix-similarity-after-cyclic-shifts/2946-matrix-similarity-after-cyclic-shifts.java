8b2a2465f1d78dcdba0bf69d81259b57b00cd694
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2026-03-27 10:39:48 +0530

    Time: 28 ms (3.2%), Space: 47.3 MB (6.67%) - LeetHub

---
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.java
@@ -0,0 +1,41 @@
+class Solution {
+    private void leftShift(int[][] arr, int row) {
+        int n = arr[0].length;
+        int temp = arr[row][0];
+        for (int i = 0; i < n-1; i++) {
+            arr[row][i] = arr[row][i+1];
+        }
+        arr[row][n-1] = temp;
+    }
+    private void rightShift(int[][] arr, int row) {
+        int n = arr[0].length;
+        int temp = arr[row][n-1];
+        for (int i = n-1; i > 0; i--) {
+            arr[row][i] = arr[row][i-1];
+        }
+        arr[row][0] = temp;
+    }
+    public boolean areSimilar(int[][] mat, int k) {
+        int n = mat.length, m = mat[0].length;
+        int[][] arr = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j =0; j < m; j++) {
+                arr[i][j] = mat[i][j];
+            }
+        }
+
+        while (k-- > 0)
+            for (int i = 0; i < n; i++) {
+                if ((i&1) == 1) rightShift(arr, i);
+                else leftShift(arr, i);
+            }
+        for (int i = 0; i < n; i++) {
+            System.out.println("MAT:- "+Arrays.toString(mat[i])+ "ARR:-"+Arrays.toString(arr[i]));
+            for (int j = 0; j < m; j++) {
+                if (mat[i][j] != arr[i][j]) return false;
+            }
+        }
+        return true;
+    }
+}
