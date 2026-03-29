2c563659d6c23e07537c7b0368365887d792506f
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2026-03-29 11:07:22 +0530

    Time: 1 ms (100%), Space: 44.9 MB (5.61%) - LeetHub

--- 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.java
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.java
@@ -1,32 +1,5 @@
 class Solution {
     public boolean canBeEqual(String s1, String s2) {
-        int n = s1.length();
-        if (s1.length() != s2.length()) return false;
-
-        int[] evenIdxChar = new int[26];
-        int[] oddIdxChar = new int[26];
-
-        for (int i = 0; i < n; i++) {
-            char currChar = s1.charAt(i);
-            if ((i&1) == 1)
-                oddIdxChar[currChar-'a']++;
-            else 
-                evenIdxChar[currChar-'a']++;
-        }
-
-        for (int i = 0; i < n; i++) {
-            char currChar = s2.charAt(i);
-            if ((i&1) == 1){
-                int val = oddIdxChar[currChar-'a'];
-                if (val < 1) return false;
-                oddIdxChar[currChar-'a']--;
-            }
-            else {
-                int val = evenIdxChar[currChar-'a'];
-                if (val < 1) return false;
-                evenIdxChar[currChar-'a']--;
-            }
-        }
-        return true;
+        return ((s1.charAt(0) == s2.charAt(0) && s1.charAt(2) == s2.charAt(2)) || (s1.charAt(0) == s2.charAt(2) && s1.charAt(2) == s2.charAt(0))) && ((s1.charAt(1) == s2.charAt(1) && s1.charAt(3) == s2.charAt(3)) || (s1.charAt(1) == s2.charAt(3) && s1.charAt(3) == s2.charAt(1)));
     }
 }
