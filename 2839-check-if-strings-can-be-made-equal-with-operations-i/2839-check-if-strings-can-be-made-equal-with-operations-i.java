c5225c7f097f4297f13e43409c149171a8f10022
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2026-03-29 10:55:50 +0530

    Time: 1 ms (100%), Space: 44.3 MB (46.94%) - LeetHub

--- 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.java
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.java
@@ -1,28 +1,30 @@
 class Solution {
     public boolean canBeEqual(String s1, String s2) {
-        Map<Character, Integer> evenIdxChar = new HashMap<>();
-        Map<Character, Integer> oddIdxChar = new HashMap<>();
         int n = s1.length();
         if (s1.length() != s2.length()) return false;
+
+        int[] evenIdxChar = new int[26];
+        int[] oddIdxChar = new int[26];
+
         for (int i = 0; i < n; i++) {
             char currChar = s1.charAt(i);
-            if ((i&1) == 1) 
-            oddIdxChar.put(currChar, oddIdxChar.getOrDefault(currChar, 0)+1);
+            if ((i&1) == 1)
+                oddIdxChar[currChar-'a']++;
             else 
-            evenIdxChar.put(currChar, evenIdxChar.getOrDefault(currChar, 0)+1);
+                evenIdxChar[currChar-'a']++;
         }
 
         for (int i = 0; i < n; i++) {
             char currChar = s2.charAt(i);
             if ((i&1) == 1){
-                int val = oddIdxChar.getOrDefault(currChar, 0);
+                int val = oddIdxChar[currChar-'a'];
                 if (val < 1) return false;
-                oddIdxChar.put(currChar, oddIdxChar.getOrDefault(currChar, 0)-1);
+                oddIdxChar[currChar-'a']--;
             }
             else {
-                int val = evenIdxChar.getOrDefault(currChar, 0);
+                int val = evenIdxChar[currChar-'a'];
                 if (val < 1) return false;
-                evenIdxChar.put(currChar, evenIdxChar.getOrDefault(currChar, 0)-1);
+                evenIdxChar[currChar-'a']--;
             }
         }
         return true;
