740deff486c222eed67c1e60bbaa96420f8f5f1d
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2026-04-20 14:08:45 +0530

    Time: 5 ms (79.59%), Space: 46.2 MB (20.65%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,14 +1,10 @@
 class Solution {
     public boolean isPalindrome(int x) {
         if (x < 0) return false;
-
         int divisor = 1;
         while (x/divisor >= 10) {
             divisor *= 10;
         }
-
-        System.out.println(divisor);
-
         while (x != 0) {
             if (x/divisor != x%10) return false;
 
