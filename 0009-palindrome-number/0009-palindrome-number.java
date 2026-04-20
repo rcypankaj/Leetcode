feda606f18ce7851b3e1052eec838ddc34d76a2b
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2026-04-20 14:21:34 +0530

    Time: 5 ms (79.59%), Space: 45.9 MB (75.53%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -6,6 +6,7 @@ class Solution {
         int xCopy = x;
         while (xCopy != 0) {
             int rem = xCopy%10;
+            if (rev > Integer.MAX_VALUE/10) return false;
             rev = (rev*10) + rem;
             xCopy /= 10;
         }
