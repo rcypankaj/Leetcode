6ece643e59b9e2839ea1e35009836cb7c37dead5
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2026-05-05 14:54:10 +0530

    Time: 0 ms (100%), Space: 44.5 MB (9.63%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -9,35 +9,32 @@
  * }
  */
 class Solution {
-    private ListNode rotateRight1(ListNode head, int k) {
-        if (head == null || head.next == null || k == 0) return head;
-        // Step 1: Calculate the size of the list
-        int size = 1; // Starts from 1 since we are already at the head
-        ListNode it = head;
-        while (it.next != null) {
-            it = it.next;
-            size++;
+    private ListNode rotateListNode(ListNode head) {
+        ListNode curr = head, prev = null;
+
+        while (curr.next != null) {
+            prev = curr;
+            curr = curr.next;
         }
-
-        // Step 2: Calculate the effective rotation needed
-        k = k%size;
-        if (k == 0) return head; // No rotation needed if k is multiple of size
-
-        // Step 3: Connect the last node to the head to form a circular list
-        it.next = head;
-
-        // Step 4: Find the new end from the list (size-k-1 starts from the head)
-        int stepsToEnd = size-k;
-        ListNode newEnd = head;
-        for (int i = 1; i < stepsToEnd; i++) newEnd = newEnd.next;
-
-        // Step 5: The new head is next to the new end, break the circle 
-        head = newEnd.next;
-        newEnd.next = null;
-
-        return head;
+        
+        prev.next = null;
+        curr.next = head;
+        return curr;
+    }
+    private int listNodeSize(ListNode head) {
+        int i = 0;
+        while (head != null) {
+            i++;
+            head = head.next;
+        }
+        return i;
     }
     public ListNode rotateRight(ListNode head, int k) {
-        return rotateRight1(head, k);
+        if (head == null || head.next == null || k == 0) return head;
+        int kTime = k%listNodeSize(head);
+        for (int i = 0; i < kTime; i++) {
+            head = rotateListNode(head);
+        }
+        return head;
     }
 }
