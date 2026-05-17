5d8d7aabd1362545afb553edeec40cfcaa27e8ac
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2026-05-17 23:07:56 +0530

    Time: 1 ms (99.73%), Space: 51.9 MB (57.54%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.java
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -1,11 +1,8 @@
 class Solution {
     private boolean dfs(int[] arr, int i) {
         if (i >= arr.length || i < 0) return false;
-
-
         if (arr[i] == 0) return true;
         if (arr[i] < 0) return false;
-
         arr[i] = -arr[i];
         
         return dfs(arr, i+arr[i]) || dfs(arr, i-arr[i]);
