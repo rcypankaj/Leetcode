82945bd6aa2827897a11c23f541c1578eca288db
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2026-05-24 19:38:10 +0530

    Time: 27 ms (6.09%), Space: 46 MB (52.86%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean isPalindrome(int x) {
         if (x < 0) return false;
-        int res = 0, temp = x;
-        while (temp != 0) {
-            int rem = temp%10;
+        int res = 0, xCopy = x;
+        while (xCopy != 0) {
+            int rem = xCopy%10;
             res = res*10+rem;
-            temp/= 10;
+            xCopy/= 10;
         }
         System.out.println(res);
         return res == x;
