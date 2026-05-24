e2fa2d1042405a9b5500537c0fe000c883de1b7c
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2026-05-25 01:54:44 +0530

    Time: 25 ms (71.64%), Space: 86.7 MB (19.39%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -44,8 +44,44 @@ class Solution {
             arr[j+1] = key;
         }
     }
+    private static void conquer(int[] arr, int si, int mid, int ei) {
+        int [] temp = new int[ei-si+1];
+        int i = si;
+        int j = mid+1;
+        int k = 0;
+
+        while (i <= mid && j <= ei) {
+            if (arr[i] <= arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+            }
+        }
+
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+        while (j <= ei) {
+            temp[k++] = arr[j++];
+        }
+
+        for (k = 0, i = si; k < temp.length; k++, i++) {
+            arr[i] = temp[k];
+        }
+    }
+    private static void divide(int[] arr, int si, int ei) {
+        if (si >= ei) return;
+        int mid = si + (ei-si)/2;
+        divide(arr, si, mid);
+        divide(arr, mid+1, ei);
+        conquer(arr, si, mid, ei);
+    }
+    private static void mergeSort(int[] arr) {
+        int n = arr.length;
+        divide(arr, 0, n-1);
+    }
     public int[] sortArray(int[] nums) {
-        insertionSort(nums);
+        mergeSort(nums);
         return nums;
     }
 }
