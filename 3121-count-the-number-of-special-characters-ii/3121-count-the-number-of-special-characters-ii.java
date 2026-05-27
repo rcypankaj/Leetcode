c5fc2960ef2e9c79234dadbfdcc685072f17f90e
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2026-05-27 12:27:05 +0530

    Time: 88 ms (8.68%), Space: 48.2 MB (34.3%) - LeetHub

---
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numberOfSpecialChars(String word) {
+        int n = word.length();
+        int[] charArr = new int[26];
+        int[] charIdx = new int[26];
+        int pair = 0;
+        for (int i = 0; i < n; i++) {
+            char currChar = word.charAt(i);
+            if(Character.isUpperCase(currChar) && charIdx[Character.toLowerCase(currChar) - 'a'] < 1) {
+                charIdx[Character.toLowerCase(currChar) - 'a'] = i+1;
+            } else if (Character.isLowerCase(currChar)) {
+                charArr[Character.toLowerCase(currChar) - 'a'] = i+1;
+            }
+        }
+        System.out.println(Arrays.toString(charIdx)+" UI"+Arrays.toString(charArr));
+
+        for (int i = 0; i < n; i++) {
+            char currChar = word.charAt(i);
+            if(Character.isUpperCase(currChar) && charArr[Character.toLowerCase(currChar) - 'a'] < charIdx[Character.toLowerCase(currChar) - 'a'] && charArr[Character.toLowerCase(currChar) - 'a'] != 0) {
+                pair++;
+                charIdx[Character.toLowerCase(currChar) - 'a'] = -1;
+            }
+        }
+        return pair;
+    }
+}
