ae7a61afd738fd2724d9d7335c295a0721cd5bbb
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2026-05-31 15:18:36 +0530

    Time: 2 ms (99.37%), Space: 47.3 MB (11.07%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,25 +1,16 @@
 class Solution {
-    // Brute force
-    private int[] findTwoSum1(int[] nums, int target) {
-        int n = nums.length;
-        for (int i = 0; i < n-1; i++) {
-            for (int j = i+1; j < n; j++) {
-                if (nums[i] + nums[j] == target) return new int[]{i, j};
-            }
-        }
-        return new int[]{};
-    }
-    // Optimal Approach
-    private int[] findTwoSum2(int[] nums, int target) {
-        int n = nums.length;
-        Map<Integer, Integer> freq = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            if (freq.containsKey(target - nums[i])) return new int[]{freq.get(target - nums[i]), i};
-            else freq.put(nums[i], i);
-        }
-        return new int[]{};
-    }
     public int[] twoSum(int[] nums, int target) {
-        return findTwoSum2(nums, target);
+        int n = nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] res = new int[2];
+        for (int i = 0; i < n; i++) {
+            if (map.containsKey(target-nums[i])) {
+                res[0] = i;
+                res[1] = map.get(target-nums[i]);
+                return res;
+            }
+            map.put(nums[i], i);
+        }
+        return res;
     }
 }
