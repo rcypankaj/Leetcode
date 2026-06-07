d20f9c9f349d5d61cfea850ae78627041e9e689c
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2026-06-07 20:19:20 +0530

    Time: 3 ms (100%), Space: 175.6 MB (28.1%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,20 +1,4 @@
 class Solution {
-    private static int findNextPosIdx(int[] nums, int i ) {
-        int n = nums.length;
-        while (i < n) {
-            if (nums[i] >= 0) return i;
-            i++;
-        }
-        return i;
-    }
-    private static int findNextNegIdx(int[] nums, int i ) {
-        int n = nums.length;
-        while (i < n) {
-            if (nums[i] < 0) return i;
-            i++;
-        }
-        return i;
-    }
     public int[] rearrangeArray(int[] nums) {
         int n = nums.length;
         int i = 0, j= 1;
