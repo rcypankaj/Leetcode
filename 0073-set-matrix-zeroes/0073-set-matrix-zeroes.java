e5d8baa7b3e9c4b6634accdeb9aca872ee76752f
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2026-06-14 20:11:23 +0530

    Time: 0 ms (100%), Space: 47.4 MB (90.47%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,15 +1,4 @@
 class Solution {
-    private void setZeroesToParticularRowCols(int[][] nums, int row, int col) {
-        int n = nums.length, m = nums[0].length;
-        for (int i = 0; i < n; i++) {
-            nums[i][col] = 0;
-        }
-
-        for (int i = 0; i < m; i++) {
-            nums[row][i] = 0;
-        }
-        
-    }
     public void setZeroes(int[][] matrix) {
         int n = matrix.length, m = matrix[0].length;
         boolean[] row = new boolean[n];
