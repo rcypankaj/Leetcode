5fb36a7140ca7553a5b3575e484a5c08c4715638
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2026-06-21 16:17:36 +0530

    Time: 4 ms (99.23%), Space: 83 MB (83.57%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -1,11 +1,23 @@
 class Solution {
     public int maxIceCream(int[] costs, int coins) {
-        Arrays.sort(costs);
-        int cost = 0, count = 0;
-        for (int num: costs) {
-            cost += num;
-            if (cost > coins) return count;
-            count++;
+        int maxCost = 0;
+        for (int cost: costs) {
+            maxCost = Math.max(maxCost, cost);
+        }
+
+        int[] arr = new int[maxCost+1];
+        for (int cost: costs) {
+            arr[cost]++;
+        }
+        int count = 0, costSpent = 0;
+        for (int i = 0; i < arr.length; i++) {
+            while (arr[i] > 0) {
+                if (arr[i] > 0)
+                    costSpent += i;
+                if (costSpent > coins) return count;
+                if (arr[i] > 0) count++;
+                arr[i]--;
+            }
         }
         return count;
     }
