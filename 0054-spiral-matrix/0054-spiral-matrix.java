572d5b1ccea408dfff67b18a8250f2078fcdf027
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2026-07-03 16:00:27 +0530

    Time: 0 ms (100%), Space: 43.1 MB (23.99%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -4,32 +4,39 @@ class Solution {
 
         List<Integer> res = new ArrayList<>();
 
-        int top = 0, bottom = n-1, left = 0, right = m-1;
+        int top = 0, bottom = n-1, left = 0, right = m-1, jump = 0;
 
         while (top <= bottom && left <= right) {
-            for (int i = left; i <= right; i++) {
-                res.add(matrix[top][i]);
-            }
-            top++;
-            
-            for (int i = top; i <= bottom; i++) {
-                res.add(matrix[i][right]);
-            }
-            right--;
+            switch (jump) {
+                case 0: 
+                    for (int i = left; i <= right; i++) {
+                        res.add(matrix[top][i]);
+                    }
+                    top++;
+                    break;
+                
+                case 1:
+                    for (int i = top; i <= bottom; i++) {
+                        res.add(matrix[i][right]);
+                    }
+                    right--;
+                    break;
 
-            if (top <= bottom) {
-                for (int i = right; i >= left; i--) {
-                    res.add(matrix[bottom][i]);
-                }
-                bottom--;
+                case 2:
+                    for (int i = right; i >= left; i--) {
+                        res.add(matrix[bottom][i]);
+                    }
+                    bottom--;
+                    break;
+
+                case 3:
+                    for (int i = bottom; i >= top; i--) {
+                        res.add(matrix[i][left]);
+                    }
+                    left++;
             }
 
-            if (right >= left) {
-                for (int i = bottom; i >= top; i--) {
-                    res.add(matrix[i][left]);
-                }
-                left++;
-            }
+            jump = (jump+1)%4;
         }
 
         return res;
