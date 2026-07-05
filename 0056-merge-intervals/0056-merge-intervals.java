9387afca3151065ca794ae58b48ed7f063dfc12d
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2026-07-05 23:45:57 +0530

    Time: 56 ms (6.02%), Space: 50.4 MB (6.02%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -7,16 +7,6 @@ class Solution {
         });
     }
 
-    // Convert List<List<Integer>> back to int[][]
-    public int[][] convertListToArray(List<List<Integer>> list) {
-        int[][] result = new int[list.size()][2];
-        for (int i = 0; i < list.size(); i++) {
-            result[i][0] = list.get(i).get(0);
-            result[i][1] = list.get(i).get(1);
-        }
-        return result;
-    }
-
     public int[][] mergeOverlappingIntervals(int[][] intervals) {
         List<List<Integer>> list = new ArrayList<>();
         list.add(new ArrayList<>(Arrays.asList(intervals[0][0], intervals[0][1])));
@@ -36,30 +26,14 @@ class Solution {
             }
         }
 
-        return convertListToArray(list);
-    }
-
-    public int[][] mergeUsingOptimalApproach(int[][] intervals) {
-        List<List<Integer>> list = new ArrayList<>();
-        int n = intervals.length;
-        // Loop through all the array
-        for (int i = 0; i < n; i++) {
-            int first = intervals[i][0];
-            int second = intervals[i][1];
-
-            // If list is empty or second is higher than last element of the intervals than continue
-            if (!list.isEmpty() && second < list.get(n-1).get(1)) continue;
-
-
-            for (int j = i+1; j < n; j++) {
-                if (intervals[i][0] <= second) {
-                    second = Math.max(intervals[j][1], second);
-                } else break;
-            }
-            list.add(Arrays.asList(first, second));
+        // Convert List<List<Integer>> back to int[][]
+        int[][] result = new int[list.size()][2];
+        for (int i = 0; i < list.size(); i++) {
+            result[i][0] = list.get(i).get(0);
+            result[i][1] = list.get(i).get(1);
         }
 
-        return convertListToArray(list);
+        return result;
     }
     public int[][] merge(int[][] intervals) {
         sortArray(intervals);
