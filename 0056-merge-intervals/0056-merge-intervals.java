86cb95c3002d24cd3955716169a015b1b14e3d88
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2026-07-07 00:55:54 +0530

    Time: 9 ms (35.59%), Space: 49.1 MB (42.25%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -5,19 +5,13 @@ class Solution {
 
         Arrays.sort(intervals, (a,b) -> a[0]-b[0]);
 
-        list.add(new int[]{intervals[0][0], intervals[0][1]});
-        for (int i = 1; i < n; i++) {
-            int start  = intervals[i][0];
-            int end  = intervals[i][1];
+        for (int i = 0; i < n; i++) {
             int listSize = list.size();
-            int prevEnd = list.get(listSize-1)[1];
-
-            if (prevEnd >= start) {
-                list.get(listSize-1)[1] = Math.max(prevEnd, end);
+            if (list.isEmpty() || list.get(listSize-1)[1] < intervals[i][0]) {
+                list.add(new int[]{intervals[i][0], intervals[i][1]});
             } else {
-                list.add(new int[]{start, end});
+                list.get(listSize-1)[1] = Math.max(list.get(listSize-1)[1], intervals[i][1]);
             }
-
         }
 
         int size = list.size();
