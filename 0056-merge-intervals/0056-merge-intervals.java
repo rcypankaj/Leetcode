9cb2ecfdc69766f64008697f8fa29720c8bcce76
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2026-07-06 19:32:28 +0530

    Time: 12 ms (9.73%), Space: 49.1 MB (42.25%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,46 +1,41 @@
 class Solution {
-    public void sortArray(int[][] intervals) {
-        Arrays.sort(intervals, new Comparator<int[]>() {
-            public int compare(int[] a, int[] b) {
-                return a[0] - b[0];
-            }
-        });
-    }
-
-    public int[][] mergeOverlappingIntervals(int[][] intervals) {
-        List<List<Integer>> list = new ArrayList<>();
-        list.add(new ArrayList<>(Arrays.asList(intervals[0][0], intervals[0][1])));
-
-        int n = intervals.length;
-
-        for (int i = 1; i < n; i++) {
-            // Get the last interval from the list
-            List<Integer> prev = list.get(list.size() - 1);
-
-            if (prev.get(1) < intervals[i][0]) {
-                // No overlap, add the current interval as a new interval
-                list.add(new ArrayList<>(Arrays.asList(intervals[i][0], intervals[i][1])));
-            } else {
-                // Overlap, merge intervals and replace the last one in the list
-                list.set(list.size() - 1, new ArrayList<>(Arrays.asList(prev.get(0), Math.max(prev.get(1), intervals[i][1]))));
-            }
-        }
-
-        // Convert List<List<Integer>> back to int[][]
-        int[][] result = new int[list.size()][2];
-        for (int i = 0; i < list.size(); i++) {
-            result[i][0] = list.get(i).get(0);
-            result[i][1] = list.get(i).get(1);
-        }
-
-        return result;
-    }
     public int[][] merge(int[][] intervals) {
-        sortArray(intervals);
-        for (int i = 0; i < intervals.length; i++) {
-            System.out.println(intervals[i][0]+" "+intervals[i][1]);
+        int n = intervals.length;
+        List<int[]> list = new ArrayList<>();
+
+        Arrays.sort(intervals, (a,b) -> a[0]-b[0]);
+
+        list.add(new int[]{intervals[0][0], intervals[0][1]});
+        for (int i = 1; i < n; i++) {
+            int start  = intervals[i][0];
+            int end  = intervals[i][1];
+            int listSize = list.size();
+            int prevStart = list.get(listSize-1)[0];
+            int prevEnd = list.get(listSize-1)[1];
+
+            if (prevEnd > start) {
+                if (prevEnd < end) {
+                    list.get(listSize-1)[1] = end;
+                } else {
+                    continue;
+                }
+            } else if (prevEnd < start) {
+                list.add(new int[]{start, end});
+            } else {
+                list.get(listSize-1)[1] = end;
+            }
+
         }
-       
-        return mergeOverlappingIntervals(intervals);
+
+        int size = list.size();
+
+        int[][] res = new int[size][2];
+
+        for (int i = 0; i < size; i++) {
+            res[i][0] = list.get(i)[0];
+            res[i][1] = list.get(i)[1];
+        }
+
+        return res;
     }
 }
