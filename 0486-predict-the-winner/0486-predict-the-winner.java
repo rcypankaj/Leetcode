9b417d51eb95fcd499515e0167a4113f198e95a1
Pankaj Yadav <113098146+rcypankaj@users.noreply.github.com>
2026-08-01 18:13:42 +0530

    Time: 1 ms (51.87%), Space: 42.6 MB (89.58%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,13 +1,23 @@
 class Solution {
-    private int solve(int[] nums, int i, int j) {
-        if (i == j) return nums[i];
-
-        int pickLeft = nums[i] - solve(nums, i+1, j);
-        int pickRight = nums[j] - solve(nums, i, j-1);
-        return Math.max(pickLeft, pickRight);
-    }
     public boolean predictTheWinner(int[] nums) {
         int n = nums.length;
-        return solve(nums, 0, n-1) >= 0;
+
+        int[][] dp = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = nums[i];
+        }
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j+i-1< n; j++) {
+                int k = i+j-1;
+                int pickLeft = nums[j]-dp[j+1][k];
+                int pickRight = nums[k]-dp[j][k-1];
+
+                dp[j][k] = Math.max(pickLeft, pickRight);
+            }
+        }
+
+        return dp[0][n-1] >= 0;
     }
 }
